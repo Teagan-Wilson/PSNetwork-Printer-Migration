93358b0a902c941f48b4f84768330d4157c29869
Teagan-Wilson <teagan.wilson@shicksolutions.com>
2016-10-13 15:06:26 -0500

    Update migrate.ps1
    Added get and search by SID.

--- migrate.ps1
+++ migrate.ps1
@@ -1,82 +1,126 @@
 #PS Network Printer Migration Script
 #Teagan Wilson
 
+Add-Type -AssemblyName System.DirectoryServices.AccountManagement
 
 #Static
-$OLDPrintServer = ""
-$NEWPrintServer = ""
-$logpath = "" + $env:computername + ".log"
+$OLDPrintServer = "STVDC"
+$NEWPrintServer = "STVPRINT1"
+$logpath = "C:" + $env:computername + ".log"
+$SID = [System.DirectoryServices.AccountManagement.UserPrincipal]::Current.Sid.Value
 #End Static
+
+
+$beginMessage = "Begining migration on " + $env:computername + " at " + (Get-Date)
+Add-Content $logpath $beginMessage
+$SIDMessage = "USER SID :" + $SID
+Add-Content $logpath $SIDMessage
 
 #functions
 function GetPrinters
 {
-$InstalledPrinters = Get-ChildItem "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Print\Providers\Client Side Rendering Print Provider\S-1-5-21-2032953301-1542742280-322568963-5881\Printers\Connections"
-$InstalledPrinters | Add-Member -Name 'PrinterName' -MemberType NoteProperty -Value ""
-Foreach ($printer in $installedPrinters)
-{
-$printer.PrinterName = $printer.name.split(",")[3]
-}
-return $InstalledPrinters
+	param( [string]$Old)
+	#Get All Network Printer Connection Paths
+	
+	$InstalledPrinters = @()
+	$combinedPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Print\Providers\Client Side Rendering Print Provider\" + $SID + "\Printers\Connections"
+    $printers = Get-ChildItem -Path $combinedPath
+		Foreach ($printer in $printers)
+		{
+		if ($Old)
+			{
+			if ($printer.Name -like "*" + $OLDPrintServer + "*")
+				{$InstalledPrinters += $printer}
+			}
+		else
+			{
+			if ($printer.Name -like "*" + $NEWPrintServer + "*")
+				{$InstalledPrinters += $printer}
+			}
+		}
+
+	
+	#Add PrinterName Attrib
+	$installedPrinters | Add-Member -Name 'PrinterName' -MemberType NoteProperty -Value ""
+	
+	#Split Printer name from the reg name and populate the Printername attrib
+    Foreach ($printer in $installedPrinters)
+    {
+        $printer.PrinterName = $printer.name.split(",")[3]
+    }
+    return $InstalledPrinters
 }
 
 function UninstallPrinter
 {
-param( [string]$Printername )
-rundll32.exe PRINTUI.DLL PrintUIEntry /gd /n\\$OLDPrintServer\$PrinterName /Gw /q
-rundll32.exe PRINTUI.DLL PrintUIEntry /dn /n\\$OLDPrintServer\$PrinterName /Gw /q
-$checkPrinters = GetPrinters
-if ($checkPrinters.Name -contains $Printername)
-{
-$errorUninstallMessage = "WARNING! Failed to uninstall: " + $Printername
-Add-Content $logpath $errorUninstallMessage
-return false
-}
-else
-{
-$errorUninstallMessage = "Uninstalled successfully: " + $Printername
-Add-Content $logpath $errorUninstallMessage
-return true}
+    param( [string]$Printername )
+	
+	#Attempt Global Removal
+	$logmessage = "rundll32.exe PRINTUI.DLL PrintUIEntry /gd /n\\$OLDPrintServer\$PrinterName /Gw /q"
+	Add-Content $logpath $logmessage
+    rundll32.exe PRINTUI.DLL PrintUIEntry /gd /n\\$OLDPrintServer\$PrinterName /Gw /q
+	
+	#Attempt Local Removal
+	$logmessage = "rundll32.exe PRINTUI.DLL PrintUIEntry /gd /n\\$OLDPrintServer\$PrinterName /Gw /q"
+	Add-Content $logpath $logmessage
+    rundll32.exe PRINTUI.DLL PrintUIEntry /dn /n\\$OLDPrintServer\$PrinterName /Gw /q
+	
+	#Validate Uninstall
+    $checkPrinters = GetPrinters -Old 1
+    if ($checkPrinters.Name -like "*" + $Printername + "*")
+    {
+        $errorUninstallMessage = "WARNING! Failed to uninstall: " + $Printername
+        Add-Content $logpath $errorUninstallMessage
+        return 0
+    }
+    else
+    {
+        $errorUninstallMessage = "Uninstalled successfully: " + $Printername
+        Add-Content $logpath $errorUninstallMessage
+    return 1}
 }
 
 function InstallPrinter
 {
-param( [string]$Printername )
-rundll32.exe PRINTUI.DLL PrintUIEntry /in /n\\$NEWPrintServer\$PrinterName /Gw /q
-$checkPrinters = GetPrinters
-if ($checkPrinters.Name -inotcontains $Printername)
-{
-$errorUninstallMessage = "WARNING! Failed to install: " + $Printername
-Add-Content $logpath $errorUninstallMessage
-return false
-}
-else
-{
-$errorUninstallMessage = "Installed successfully: " + $Printername
-Add-Content $logpath $errorUninstallMessage
-return true
-}
+    param( [string]$Printername )
+    #Attempt Install
+	$logmessage = "rundll32.exe PRINTUI.DLL PrintUIEntry /in /n\\$NEWPrintServer\$PrinterName /Gw /q"
+	Add-Content $logpath $logmessage
+    rundll32.exe PRINTUI.DLL PrintUIEntry /in /n\\$NEWPrintServer\$PrinterName /Gw /q
+    $checkPrinters = GetPrinters -Old 0
+    if ($checkPrinters.Name -like "*" + $Printername + "*")
+    {
+        $errorUninstallMessage = "WARNING! Failed to install: " + $Printername
+        Add-Content $logpath $errorUninstallMessage
+        return 0
+    }
+    else
+    {
+        $errorUninstallMessage = "Installed successfully: " + $Printername
+        Add-Content $logpath $errorUninstallMessage
+        return 1
+    }
 }
 #end functions
 
 #Uninstall
+$InstalledPrinters = GetPrinters -Old 1
+Add-Content $logpath $InstalledPrinters
 Foreach ($printer in $InstalledPrinters)
 {
-$PrinterName = $printer.PrinterName
-$uninstallMessage = "Attempting Uninstall of :" + $PrinterName
-Add-Content $logpath $uninstallMessage
-$value = UninstallPrinter -Printername $PrinterName
-if ($value)
-{} else {UninstallPrinter -Printername $PrinterName}
+    $PrinterName = $printer.PrinterName
+    $uninstallMessage = "Attempting Uninstall of :" + $PrinterName
+    Add-Content $logpath $uninstallMessage
+    $value = UninstallPrinter -Printername $PrinterName
+    if ($value)
+    {} else {UninstallPrinter -Printername $PrinterName}
 }
 
 #Install
 Foreach ($printer in $InstalledPrinters)
 {
-$installMessage = "Attempting Install of :" + $PrinterName
-Add-Content $logpath $installMessage
-$PrinterName = $printer.PrinterName
-InstallPrinter -PrinterName $PrinterName
+    $installMessage = "Attempting Install of :" + $PrinterName
+    Add-Content $logpath $installMessage
+    $PrinterName = $printer.PrinterName
+    InstallPrinter -PrinterName $PrinterName
 }
-
-
