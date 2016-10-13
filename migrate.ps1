e33eb51494862c672c4b6d51239c5d3bd3545158
Teagan-Wilson <teagan.wilson@shicksolutions.com>
2016-10-13 11:18:52 -0500

    Update migrate.ps1
    - Overhauled Functions
    - Added logging (Failure,Success,On-action)

--- migrate.ps1
+++ migrate.ps1
@@ -1,48 +1,82 @@
-
 #PS Network Printer Migration Script
 #Teagan Wilson
-#Note: Enviroment Specific, Not for general consumption, without major customization.
 
-#Statics
+
+#Static
 $OLDPrintServer = ""
 $NEWPrintServer = ""
+$logpath = "" + $env:computername + ".log"
+#End Static
 
-
-#Get installed network printers.
+#functions
+function GetPrinters
+{
 $InstalledPrinters = Get-ChildItem "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Print\Providers\Client Side Rendering Print Provider\S-1-5-21-2032953301-1542742280-322568963-5881\Printers\Connections"
 $InstalledPrinters | Add-Member -Name 'PrinterName' -MemberType NoteProperty -Value ""
 Foreach ($printer in $installedPrinters)
 {
 $printer.PrinterName = $printer.name.split(",")[3]
 }
-$InstalledPrinters.PrinterName
+return $InstalledPrinters
+}
 
-#Get Default Printer
-$DefaultPRN = Get-WmiObject -Query " SELECT * FROM Win32_Printer WHERE Default=$true"
-$DefaultPRN | Add-Member -Name 'PrinterName' -MemberType NoteProperty -Value ""
-
-#Filter path by slashes
-$DefaultPRN.PrinterName = $DefaultPRN.Name.Split("\")[3]
-
-#Just checking for logging sake
-if ($InstalledPrinters.PrinterName -contains $DefaultPRN.PrinterName)
+function UninstallPrinter
 {
-write-host "Default is network printer"
+param( [string]$Printername )
+rundll32.exe PRINTUI.DLL PrintUIEntry /gd /n\\$OLDPrintServer\$PrinterName /Gw /q
+rundll32.exe PRINTUI.DLL PrintUIEntry /dn /n\\$OLDPrintServer\$PrinterName /Gw /q
+$checkPrinters = GetPrinters
+if ($checkPrinters.Name -contains $Printername)
+{
+$errorUninstallMessage = "WARNING! Failed to uninstall: " + $Printername
+Add-Content $logpath $errorUninstallMessage
+return false
 }
 else
 {
-Write-Host "Default is not a network printer"
+$errorUninstallMessage = "Uninstalled successfully: " + $Printername
+Add-Content $logpath $errorUninstallMessage
+return true}
 }
 
+function InstallPrinter
+{
+param( [string]$Printername )
+rundll32.exe PRINTUI.DLL PrintUIEntry /in /n\\$NEWPrintServer\$PrinterName /Gw /q
+$checkPrinters = GetPrinters
+if ($checkPrinters.Name -inotcontains $Printername)
+{
+$errorUninstallMessage = "WARNING! Failed to install: " + $Printername
+Add-Content $logpath $errorUninstallMessage
+return false
+}
+else
+{
+$errorUninstallMessage = "Installed successfully: " + $Printername
+Add-Content $logpath $errorUninstallMessage
+return true
+}
+}
+#end functions
+
+#Uninstall
 Foreach ($printer in $InstalledPrinters)
 {
 $PrinterName = $printer.PrinterName
-rundll32.exe PRINTUI.DLL PrintUIEntry /dn /n\\$OLDPrintServer\$PrinterName Gw /q
+$uninstallMessage = "Attempting Uninstall of :" + $PrinterName
+Add-Content $logpath $uninstallMessage
+$value = UninstallPrinter -Printername $PrinterName
+if ($value)
+{} else {UninstallPrinter -Printername $PrinterName}
 }
 
+#Install
 Foreach ($printer in $InstalledPrinters)
 {
+$installMessage = "Attempting Install of :" + $PrinterName
+Add-Content $logpath $installMessage
 $PrinterName = $printer.PrinterName
-rundll32.exe PRINTUI.DLL PrintUIEntry /ga /n\\$NEWPrintServer\$printer.PrinterName Gw /q
+InstallPrinter -PrinterName $PrinterName
 }
+
 
